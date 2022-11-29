a214744d3a59f35e88e15dcd5ab1ffac1193c024
slava.vlasov <slava.vlasov@support-partners.com>
2022-11-29 12:36:53 +0300

    Init commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      version = "0.82.0"
+    }
+  }
+
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "terraform-task-01"
+    region     = "ru-central1"
+    key        = "terraform/terraform.tfstate"
+    access_key = ""
+    secret_key = ""
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
+
+provider "yandex" {
+  token     = ""
+  cloud_id  = "vpc01"
+  folder_id = "b1ggn66vp33rf2c84cpm"
+  zone      = "ru-central1-a"
+}
